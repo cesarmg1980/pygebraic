3b6203e2991295c72407c457fc919bac511e0e5f
cesarmg1980 <cesar.m.giulietti@outlook.com>
2020-04-19 18:18:17 -0300

    changed algebraic term to use regex to parse terms with exponents

--- code/base/base.py
+++ code/base/base.py
@@ -1,19 +1,30 @@
+import re
+
 class AlgebraicTerm():
+    COEFICIENT_PATTERN = r'^\d+(?=[a-zA-Z])'
+    LITERALS_PATTERN = r'([a-zA-Z]\^\d+)|([a-zA-Z])'
+    
     def __init__(self, expression):
-        self.coeficient = 1
+        coef = re.search(self.COEFICIENT_PATTERN, expression)
+        self.coeficient = int(coef.group()) if coef is not None else 1
+        literals = re.findall(self.LITERALS_PATTERN, expression)
         self.literals = {}
-        self.component_list = [e for e in expression]
         literal_index = 0
-        for elem in self.component_list:
-            if elem.isdigit():
-                self.coeficient *= int(elem)
-            elif elem.isalpha():
+
+        for elem in literals:
+            if elem[0] is not '':
+                literal_key = 'literal_' + str(literal_index) 
+                self.literals[literal_key] = {}
+                lit, exp = elem[0].split('^')
+                self.literals[literal_key]['literal'] = lit
+                self.literals[literal_key]['exponent'] = int(exp)
+
+            else:
                 literal_key = 'literal_' + str(literal_index)
-                self.literals[literal_key] = elem
-                literal_index += 1
+                self.literals[literal_key] = {}
+                self.literals[literal_key]['literal'] = elem[1]
+                self.literals[literal_key]['exponent'] = 1
+            literal_index += 1
 
     def __str__(self):
-        str_value = str(self.coeficient) if self.coeficient is not 1 else ''
-        for v in self.literals.values():
-            str_value += v
-        return str_value
+        pass
--- test/basic_operations/test_sum.py
+++ test/basic_operations/test_sum.py
@@ -7,8 +7,15 @@ from code.base.base import AlgebraicTerm
 
 class TestAlgebraicTerm(unittest.TestCase):
     def test_algebraic_expression_is_properly_created(self):
-        term = AlgebraicTerm('5x2y2j')
-        self.assertEqual(str(term), '20xyj')
+        term = AlgebraicTerm('20x^2yj')
+        self.assertEqual(term.coeficient, 20)
+        self.assertEqual(term.literals['literal_0']['literal'], 'x')
+        self.assertEqual(term.literals['literal_0']['exponent'], 2)
+        self.assertEqual(term.literals['literal_1']['literal'], 'y')
+        self.assertEqual(term.literals['literal_1']['exponent'], 1)
+        self.assertEqual(term.literals['literal_2']['literal'], 'j')
+        self.assertEqual(term.literals['literal_2']['exponent'], 1)
+        self.assertEqual(str(term), '20x^2yj')
 
 
 class TestAlgebraicSum(unittest.TestCase):
