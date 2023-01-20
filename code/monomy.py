7573dd478b9f33f268bf18f682974d8e7d792f0f
cesarmg1980 <cesar.m.giulietti@outlook.com>
2023-01-20 19:29:46 -0300

    fixed failing tests, added isort, black and mypy, implemented __eq__

--- code/monomy.py
+++ code/monomy.py
@@ -19,9 +19,7 @@ class Monomy:
         self._build_monomy(expression)
 
     def _build_monomy(self, expression):
-        coefficient = re.search(
-            COEFFICIENT_PATTERN, expression
-        )
+        coefficient = re.search(COEFFICIENT_PATTERN, expression)
         if coefficient.group() is ONE:
             self.coefficient = 1
         elif coefficient.group() is MINUS_ONE:
@@ -29,31 +27,21 @@ class Monomy:
         else:
             self.coefficient = int(coefficient.group())
 
-        literals = re.findall(
-            LITERALS_PATTERN, expression
-        )
+        literals = re.findall(LITERALS_PATTERN, expression)
         if literals is None:
             raise MonomicExpressionWithoutLiteralsError
 
         self.literals_list = [item for item in literals]
 
-
         """the exponent list also has the exponent's literal
         this is done so you can know to what literal
         an exponent belongs
         """
-        exponents_list = re.findall(
-            EXPONENT_PATTERN,
-            expression
-        )
-        exponents_dict = self._sanitize_exponents_list_into_dict(
-            exponents_list
-        )
+        exponents_list = re.findall(EXPONENT_PATTERN, expression)
+        exponents_dict = self._sanitize_exponents_list_into_dict(exponents_list)
 
         for literal in self.literals_list:
-            self.literal_to_exponent_mapping[literal] = \
-                    exponents_dict.get(literal, 1)
-
+            self.literal_to_exponent_mapping[literal] = exponents_dict.get(literal, 1)
 
     def _sanitize_exponents_list_into_dict(self, exponents_list):
         """
@@ -64,25 +52,50 @@ class Monomy:
         literal_exponent_dict = {}
 
         for element in exponents_list:
-            literal_exponent_pair = element.split('^')
-            literal_exponent_dict[literal_exponent_pair[0]] = int(literal_exponent_pair[1])
+            literal_exponent_pair = element.split("^")
+            literal_exponent_dict[literal_exponent_pair[0]] = int(
+                literal_exponent_pair[1]
+            )
 
         return literal_exponent_dict
 
     def _literal_to_exponent_mapping_to_string(self) -> str:
         """Converts the 'literal_to_exponent_mapping' into a readable string"""
-        return "".join([f"{k}^{v}" if v > 1 else f"{k}" for k, v in self.literal_to_exponent_mapping.items()])
+        return "".join(
+            [
+                f"{k}^{v}" if v > 1 else f"{k}"
+                for k, v in self.literal_to_exponent_mapping.items()
+            ]
+        )
 
-    def __add__(self, other: 'Monomy') -> 'Monomy':
+    def _coefficient_to_string(self) -> str:
+        """Converts the Monomy Coefficient to readable string"""
+        if self.coefficient == 1:
+            return ONE
+        elif self.coefficient == -1:
+            return MINUS_ONE
+        else:
+            return str(self.coefficient)
+
+    def __add__(self, other: "Monomy") -> "Monomy":
         """Adds 2 Monomies using the '+' operator"""
         pass
 
-    def __eq__(self, other: 'Monomy') -> bool:
+    def __eq__(self, other: object) -> bool:
         """Compares 2 Monomies"""
-        pass
-
+        if not isinstance(other, Monomy):
+            raise Exception("Error: <other> is not of a 'Monomy' instance")
+        return (
+            self.coefficient == other.coefficient
+            and self.literals_list == other.literals_list
+            and self.literal_to_exponent_mapping == other.literal_to_exponent_mapping
+        )
 
     def __repr__(self) -> str:
         """Returns the textual representation of a Monomy"""
-        literals_str = self._literal_to_exponent_mapping_to_string()
-        return "".join([str(self.coefficient), literals_str])
+        return "".join(
+            [
+                self._coefficient_to_string(),
+                self._literal_to_exponent_mapping_to_string(),
+            ]
+        )
--- tests/test_monomy.py
+++ tests/test_monomy.py
@@ -1,49 +1,75 @@
 from code.monomy import Monomy
 
+import pytest
+
+
 def test_monomy_with_no_coefficient_and_no_exponents_is_correctly_build():
-    monomy = Monomy('abc')
+    monomy = Monomy("abc")
 
     assert monomy.coefficient == 1
-    assert monomy.literals_list == ['a', 'b', 'c']
-    assert monomy.literal_to_exponent_mapping == {'a': 1, 'b': 1, 'c': 1}
-    assert monomy == 'abc'
+    assert monomy.literals_list == ["a", "b", "c"]
+    assert monomy.literal_to_exponent_mapping == {"a": 1, "b": 1, "c": 1}
+    assert "abc" == f"{monomy}"
+
 
 def test_monomy_with_coefficient_is_correctly_build():
-    monomy = Monomy('2xyz')
+    monomy = Monomy("2xyz")
 
-    assert monomy.coefficient ==  2
-    assert monomy.literals_list == ['x', 'y', 'z']
-    assert monomy.literal_to_exponent_mapping == {'x': 1, 'y': 1, 'z': 1}
-    assert monomy == '2xyz'
+    assert monomy.coefficient == 2
+    assert monomy.literals_list == ["x", "y", "z"]
+    assert monomy.literal_to_exponent_mapping == {"x": 1, "y": 1, "z": 1}
+    assert "2xyz" == f"{monomy}"
+
 
 def test_monomy_with_coefficient_and_exponents_is_correctly_build():
-    monomy = Monomy('3a^2b^3c')
+    monomy = Monomy("3a^2b^3c")
 
     assert monomy.coefficient == 3
-    assert monomy.literals_list == ['a', 'b', 'c']
-    assert monomy.literal_to_exponent_mapping == {'a': 2, 'b': 3, 'c': 1}
-    assert monomy == '3a^2b^3c'
+    assert monomy.literals_list == ["a", "b", "c"]
+    assert monomy.literal_to_exponent_mapping == {"a": 2, "b": 3, "c": 1}
+    assert "3a^2b^3c" == f"{monomy}"
+
 
 def test_monomy_with_negative_1_coefficient():
-    monomy = Monomy('-abc')
+    monomy = Monomy("-abc")
 
     assert monomy.coefficient == -1
-    assert monomy.literals_list == ['a', 'b', 'c']
-    assert monomy.literal_to_exponent_mapping == {'a': 1, 'b': 1, 'c': 1}
-    assert monomy == '-abc'
+    assert monomy.literals_list == ["a", "b", "c"]
+    assert monomy.literal_to_exponent_mapping == {"a": 1, "b": 1, "c": 1}
+    assert "-abc" == f"{monomy}"
+
 
 def test_monomy_with_negative_coefficient_different_than_1():
-    monomy = Monomy('-2xyz^2')
+    monomy = Monomy("-2xyz^2")
 
     assert monomy.coefficient == -2
-    assert monomy.literals_list == ['x', 'y', 'z']
-    assert monomy.literal_to_exponent_mapping == {'x': 1, 'y': 1, 'z': 2}
-    assert monomy == '-2xyz^2'
+    assert monomy.literals_list == ["x", "y", "z"]
+    assert monomy.literal_to_exponent_mapping == {"x": 1, "y": 1, "z": 2}
+    assert "-2xyz^2" == f"{monomy}"
 
-def test_monomy_sum():
-    monomy_1 = Monomy('2a')
-    monomy_2 = Monomy('a')
+
+def test_two_monomies_are_equal():
+    monomy_1 = Monomy("3xy")
+    monomy_2 = Monomy("3xy")
+
+    assert monomy_1 == monomy_2
+
+
+@pytest.mark.skip(reason="Not Implemented Yet")
+def test_alike_monomy_sum():
+    monomy_1 = Monomy("a")
+    monomy_2 = Monomy("a")
 
     result = monomy_1 + monomy_2
 
-    assert result == '3a'
+    assert result == "2a"
+
+
+@pytest.mark.skip(reason="Not Implemented Yet")
+def test_not_alike_monomy_sum():
+    monomy_1 = Monomy("2a")
+    monomy_2 = Monomy("b")
+
+    result = monomy_1 + monomy_2
+
+    assert result == "2a+b"
