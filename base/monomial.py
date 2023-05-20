713e9c126add31aacf83cd0fee56c3a9a348fd30
cesarmg1980 <cesar.m.giulietti@outlook.com>
2023-05-20 17:20:29 -0300

    implemented monomial division

--- base/monomial.py
+++ base/monomial.py
@@ -70,6 +70,14 @@ class Monomial:
 
         return variable_exponent_dict
 
+    def __eq__(self, other: object) -> bool:
+        """Compares 2 Monomies"""
+        if not isinstance(other, Monomial):
+            raise Exception("Error: <other> is not of a 'Monomial' instance")
+        return (
+            self.coefficient == other.coefficient and self.variables == other.variables
+        )
+
     def __add__(self, other: "Monomial") -> "Monomial":
         """Adds 2 Monomies using the '+' operator"""
         if not self._alike_monomies(other):
@@ -92,18 +100,31 @@ class Monomial:
         """
         Multiplies 2 Monomials
         """
-        return Monomial(
+        result = Monomial(
             self.coefficient * other.coefficient,
             dict(Counter(self.variables) + Counter(other.variables)),
         )
+        return result
 
-    def __eq__(self, other: object) -> bool:
-        """Compares 2 Monomies"""
-        if not isinstance(other, Monomial):
-            raise Exception("Error: <other> is not of a 'Monomial' instance")
-        return (
-            self.coefficient == other.coefficient and self.variables == other.variables
+    def __truediv__(self, other: "Monomial") -> "Monomial":
+        """
+        Divides 2 Monomials
+        """
+        raw_coefficient_division = (
+            self.coefficient / other.coefficient
+        )  # here we get a float
+        result = (
+            int(self.coefficient / other.coefficient)
+            if raw_coefficient_division.is_integer()
+            else self.coefficient / other.coefficient
+        )  # here we remove the decimal part if the result is an int and we leave it if it's not
+        result = Monomial(
+            result,
+            self._sanitize_when_exponent_is_zero(
+                dict(Counter(self.variables) - Counter(other.variables))
+            ),
         )
+        return result
 
     def __repr__(self) -> str:
         """Returns the textual representation of a Monomial"""
@@ -133,3 +154,14 @@ class Monomial:
             return MINUS_ONE
         else:
             return str(self.coefficient)
+
+    def _sanitize_when_exponent_is_zero(self, variables: dict) -> None:
+        """
+        Method used specially in Monimial Division, as we all know when two
+        Monomials are divided their literal's exponents are substracted, if the
+        result of that substraction is 0 then the literal 'disappears' (sorry,
+        this is probably not mathematically correct to say), i.e 4ab / 2a = 2b
+        """
+        self.variables = {
+            key: value for key, value in variables.items() if not value == 0
+        }
